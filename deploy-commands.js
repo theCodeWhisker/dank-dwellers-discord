22a77a3bace9d19dfb653dd629fbce04abd22eb4
theCodeWhisker <admin@catonbox.com>
2023-12-07 23:00:57 -0600

    Fix error in deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,9 +1,33 @@
 require("dotenv").config();
 const { REST, Routes } = require("discord.js");
 const { CLIENT_ID, GUILD_ID, TOKEN } = process.env;
-const { bot } = require(".");
+const fs = require("node:fs");
+const path = require("node:path");
 
-const commands = bot.commands;
+const commands = [];
+// Grab all the command folders from the commands directory you created earlier
+const foldersPath = path.join(process.cwd(), "commands");
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+  // Grab all the command files from the commands directory you created earlier
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith(".js"));
+  // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    if ("data" in command && "execute" in command) {
+      commands.push(command.data.toJSON());
+    } else {
+      console.log(
+        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+      );
+    }
+  }
+}
 
 // Construct and prepare an instance of the REST module
 const rest = new REST().setToken(TOKEN);
@@ -26,6 +50,6 @@ const rest = new REST().setToken(TOKEN);
     );
   } catch (error) {
     // And of course, make sure you catch and log any errors!
-    console.error(error.message);
+    console.error(error);
   }
 })();
