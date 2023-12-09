c11326403dfa0ceed838fe119260f741e181a75b
theCodeWhisker <admin@catonbox.com>
2023-12-09 03:51:04 -0600

    Made some design changes to the Bot model

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const { Bot } = require("./models/Bot");
 
 const bot = new Bot();
 
+bot.login(bot.token);
+
+bot.on("ready", () => {
+  console.log(`Bot is running as ${bot.user.tag}`);
+});
+
 module.exports = {
   bot,
 };
--- models/Bot.js
+++ models/Bot.js
@@ -7,35 +7,27 @@ const {
 } = require("discord.js");
 const fs = require("node:fs");
 const path = require("node:path");
+const logger = require("../utils/logger");
 
 class Bot extends Client {
   commands = new Collection();
 
-  constructor(
-    props = {
+  constructor(options = {}) {
+    super({
       intents: [
         IntentsBitField.Flags.Guilds,
         IntentsBitField.Flags.MessageContent,
         IntentsBitField.Flags.GuildMessages,
         IntentsBitField.Flags.GuildEmojisAndStickers,
         IntentsBitField.Flags.GuildVoiceStates,
-        IntentsBitField.Flags.GuildBans,
         GatewayIntentBits.Guilds,
       ],
-      token: process.env.TOKEN,
-    }
-  ) {
-    super(props);
-
-    this.login(props.token);
-
-    this.on("ready", () => {
-      console.log(`Bot is running as ${this.user.tag}`);
     });
 
-    this.loadCommands();
+    this.token = options.token || process.env.TOKEN;
 
-    this.on(Events.InteractionCreate, this.executeCommand);
+    this.loadCommands();
+    this.registerEventHandlers();
   }
 
   loadCommands() {
@@ -62,11 +54,15 @@ class Bot extends Client {
     }
   }
 
+  registerEventHandlers() {
+    this.on(Events.InteractionCreate, this.handleInteraction);
+  }
+
   get commands() {
     return this.commands;
   }
 
-  async executeCommand(interaction) {
+  async handleInteraction(interaction) {
     const command = interaction.client.commands.get(interaction.commandName);
 
     if (!command) {
@@ -95,11 +91,41 @@ class Bot extends Client {
     }
   }
 
-  async getUser(interaction) {
-    if (!interaction) return;
-    if (!interaction.member) return;
+  async getGuildMember(interaction) {
+    if (!interaction || !interaction.member) return null;
 
     return interaction.member;
+  }
+
+  sendHelpMessage(channel) {
+    const helpMessage = this.commands
+      .map((command) => `${command.name}: ${command.description}`)
+      .join("\n");
+    channel.send(helpMessage);
+  }
+
+  sendUserFeedback(interaction, message) {
+    interaction.reply(message);
+  }
+
+  logEvent(eventType, details) {
+    if (typeof details !== "string")
+      return logger.error("Message must be a string when logging.");
+    if (eventType === "error") return logger.error(details);
+    if (eventType === "warn") return logger.warn(details);
+    else return logger.info(details);
+  }
+
+  getServerInfo(guild) {
+    // Return server information
+  }
+
+  trackUserActivity(user, activityType) {
+    // Implementation to track user activity
+  }
+
+  scheduleMessage(channel, message, time) {
+    // Schedule a message to be sent
   }
 }
 
